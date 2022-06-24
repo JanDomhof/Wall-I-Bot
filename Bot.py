e537a3e0a03e365c0c047d3127cc4cc94be7460a
jandomhof <59569002+JanDomhof@users.noreply.github.com>
2022-06-24 13:56:20 +0000

    created points system

--- Bot.py
+++ Bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import json
 from discord.ext.commands import Bot
 
 import MemeBot
@@ -14,7 +15,6 @@ async def on_ready():
 
 @client.command()
 async def embed(ctx):
-  print('displayembed')
   embed = discord.Embed(
     title = 'Title',
     description = 'This is the description',
@@ -24,32 +24,50 @@ async def embed(ctx):
   embed.set_footer(
     text="This is the footer.", 
     icon_url='https://pbs.twimg.com/media/CmnU0PGWIAE857G.jpg')
-  
   embed.set_image(url='https://cdn.dribbble.com/users/226242/screenshots/2767743/wall_e.png')
   embed.set_author(
-    name=ctx.message.author, 
+    name=ctx.message.author,
     url=ctx.message.author.avatar_url,
   icon_url=ctx.message.author.avatar_url)
 
   await ctx.send(embed=embed)
 
+@client.command()
+async def work(ctx):
+  with open('users.json', 'r') as f:
+    users = json.load(f)
+
+  await PointsBot.add_work(users, ctx.message.author, ctx.message.channel, 10)
+
+  with open('users.json', 'w') as f:
+    json.dump(users, f)
+
+@client.event
+async def on_member_join(member):
+  with open('users.json', 'r') as f:
+    users = json.load(f)
+
+  await PointsBot.updata_data(users, member)
+
+  with open('users.json', 'w') as f:
+    json.dump(users, f)
+    
+    
+  
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
+      
     await client.process_commands(message)
 
     # messages that start with '$' are commands related to the MemeBot
     if message.content.startswith('$'):
         await MemeBot.handle_message(message, client)
 
-    # messages that start with '!' are commands related to the PointsBot
-    if message.content.startswith('!'):
-        await PointsBot.handle_message(message, client)
-
     # messages that start with '#' are commands related to the MessageBot
     if message.content.startswith('#'):
         await MessageBot.handle_message(message, client)
-    
+
 client.run(os.environ['envDiscord'])
--- PointsBot.py
+++ PointsBot.py
@@ -1,17 +1,83 @@
-import discord
-from replit import db
+import datetime
+import random
 
-async def handle_message(message, client):
-    print("PointsBot message recieved")
+jobs = [
+  {
+    'description': 'You helped an old women cross the street!',
+    'salary': 43
+  },
+  {
+    'description': 'You taught a class about crypto!',
+    'salary': 54
+  },
+  {
+    'description': 'You traded an NFT!',
+    'salary': 73
+  },
+  {
+    'description': 'You defended an innocent person in court!',
+    'salary': 67
+  },
+  {
+    'description': 'You delivered mail to 31 apartments!',
+    'salary': 31
+  },
+  {
+    'description': 'You won a raffle!',
+    'salary': 100
+  },
+  {
+    'description': 'You developed a smart contract!',
+    'salary': 83
+  },
+]
 
-    author = f"{message.author}"
 
-    if message.content.startswith("$give point"):
-        if author in db.keys():
-            db[author] = db[author] + 1
-            print("Added 1 point to {}, Current points of {} are {}".format(author, author, db[author]))
-        else:
-            db[author] = 1
+async def update_data(users, user):
+    if not user.id in users:
+        new_user = {}
+        new_user['points'] = 0
+        new_user['updated'] = datetime.datetime.now().replace(
+            hour=0, minute=0, second=0, microsecond=0).isoformat()
+        users[str(user.id)] = new_user
 
-    if message.content.startswith("$show points"):
-        await message.channel.send("total points of {} are / is {}".format(author, db[author]))
+
+async def add_work(users, user, channel, timeout):
+    if not str(user.id) in users:
+        await update_data(users, user)
+
+    key = str(user.id)
+    updated = datetime.datetime.fromisoformat(users[key]['updated'])
+
+    time_passed = (datetime.datetime.now() - updated).total_seconds()
+
+    if time_passed > timeout:
+        job = random.choice(jobs)
+        users[key]['points'] += job['salary']
+        users[key]['updated'] = datetime.datetime.now().isoformat()
+
+
+
+        embed = discord.Embed(
+          title = 'Title',
+          description = 'This is the description',
+          colour = discord.Colour.blue(),
+        )
+      
+        embed.set_footer(
+          text="This is the footer.", 
+          icon_url='https://pbs.twimg.com/media/CmnU0PGWIAE857G.jpg')
+        embed.set_image(url='https://cdn.dribbble.com/users/226242/screenshots/2767743/wall_e.png')
+        embed.set_author(
+          name=ctx.message.author,
+          url=ctx.message.author.avatar_url,
+        icon_url=ctx.message.author.avatar_url)
+
+  await ctx.send(embed=embed)
+      
+        message = job['description'] + " You earned {} points!".format(job['salary'])
+      
+        await channel.send(message + " You have a total of {} points!".format(users[key]['points']))
+    else:
+        await channel.send(
+            'You can work again in {} seconds'.format(timeout - time_passed))
