29dc43ee519bedeb3e9e939f91db36c00075e6c0
jandomhof <j.h.domhof@outlook.com>
2022-07-05 23:48:32 +0200

    completed shift to Cogs

--- Bot.py
+++ Bot.py
@@ -3,9 +3,6 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv, find_dotenv
 
-import MemeBot
-import MessageBot
-
 load_dotenv(find_dotenv())
 client = commands.Bot('!')
 
@@ -25,41 +22,6 @@ async def unload(ctx, extension):
 async def reload(ctx, extension):
   client.unload_extension(f'cogs.{extension}')
   client.load_extension(f'cogs.{extension}')
-
-@client.command()
-async def embed(ctx):
-  print('displayembed')
-  embed = discord.Embed(
-    title = 'Title',
-    description = 'This is the description',
-    colour = discord.Colour.blue(),
-  )
-  embed.set_footer(
-    text="This is the footer.", 
-    icon_url='https://pbs.twimg.com/media/CmnU0PGWIAE857G.jpg'
-  )
-  embed.set_image(url='https://cdn.dribbble.com/users/226242/screenshots/2767743/wall_e.png')
-  embed.set_author(
-    name=ctx.message.author, 
-    url=ctx.message.author.avatar_url,
-    icon_url=ctx.message.author.avatar_url
-  )
-  await ctx.send(embed=embed)
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    await client.process_commands(message)
-
-    # messages that start with '$' are commands related to the MemeBot
-    if message.content.startswith('$'):
-        await MemeBot.handle_message(message, client)
-
-    # messages that start with '#' are commands related to the MessageBot
-    if message.content.startswith('#'):
-        await MessageBot.handle_message(message, client)
     
 for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
---
+++ cogs/MemeCog.py
@@ -0,0 +1,59 @@
+import discord
+import json
+import os
+from discord.ext import commands
+
+def wrong_name_embed(user, meme):
+  embed = discord.Embed(
+    title = 'There is no meme named {}!'.format(meme),
+    colour = discord.Colour.red(),
+  )
+
+  embed.set_author(
+    name=user,
+    url=user.avatar_url,
+    icon_url=user.avatar_url
+  )
+
+  return embed
+
+def error_embed(user, error):
+  embed = discord.Embed(
+    title = error,
+    colour = discord.Colour.red(),
+  )
+
+  embed.set_author(
+    name=user,
+    url=user.avatar_url,
+    icon_url=user.avatar_url
+  )
+
+  return embed
+
+class MemeCog(commands.Cog):
+  def __init__(self, client):
+    self.client = client
+
+  @commands.Cog.listener()
+  async def on_ready(self):
+      print("MemeCog loaded.")
+
+  @commands.command()
+  async def meme(self, ctx, meme):
+    file = None
+    for filename in os.listdir('./memes'):
+      if filename[:-4] == meme:
+        file = filename
+    if file is not None:
+      await ctx.message.channel.send(file=discord.File('./memes/{}'.format(file)))
+    else:
+      await ctx.message.channel.send(embed=wrong_name_embed(ctx.message.author, meme))
+
+  @meme.error
+  async def missing_argument(self, ctx, error):
+    await ctx.message.channel.send(embed=error_embed(ctx.message.author, error))
+
+
+def setup(client):
+  client.add_cog(MemeCog(client))
