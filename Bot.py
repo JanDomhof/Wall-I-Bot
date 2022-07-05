08169690bf081c2bc0fa7c8288647a1dbfc339bd
jandomhof <j.h.domhof@outlook.com>
2022-07-05 23:50:20 +0200

    removed unnessesary lines

--- Bot.py
+++ Bot.py
@@ -9,19 +9,6 @@ client = commands.Bot('!')
 @client.event
 async def on_ready():
   print("Logged in as {0.user}".format(client))
-
-@client.command()
-async def load(ctx, extension):
-  client.load_extension(f'cogs.{extension}')
-
-@client.command()
-async def unload(ctx, extension):
-  client.unload_extension(f'cogs.{extension}')
-
-@client.command()
-async def reload(ctx, extension):
-  client.unload_extension(f'cogs.{extension}')
-  client.load_extension(f'cogs.{extension}')
     
 for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
